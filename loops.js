2c3a7f3d0dafbf1134dac94011ccc1f030faccac
Anna Conaway <annaconaway19@gmail.com>
2018-08-01 13:53:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,20 @@
 function forLoop(array) {
 for (let i = 0; i < 25; i++) {
   if (i === 1) {
+<<<<<<< HEAD
     array.push("I am 1 strange loop.");
   } else {
     array.push(`I am ${i} strange loops.`);
   }
 }
 return array;
+=======
+    return "I am 1 strange loop.";
+  } else {
+    return `I am ${i} strange loops.`;
+  }
+}
+>>>>>>> e4dbe515bae5db4de2a3afbad47df6ac88765707
 }
 
 function whileLoop(number) {
@@ -16,6 +24,7 @@ function whileLoop(number) {
   return 'done';
 }
 
+<<<<<<< HEAD
 function maybeTrue() {
   return Math.random() >= 0.5
 }
@@ -24,3 +33,9 @@ function doWhileLoop(array) {
       } while (array.length > 0 && maybeTrue())
 return array;
 }
+=======
+function doWhileLoop(array) {
+  do { --array;
+      } while (array.length > 0 && maybeTrue())
+}
+>>>>>>> e4dbe515bae5db4de2a3afbad47df6ac88765707
